366e7cdd6026447de3a9377c0b3eecb972c2fcce
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-05-12 14:08:34 +0100

    home page started

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,15 @@ import React from 'react';
 const Home = () => {
     return ( 
         <div className='home-container'>
-            <h3>Hi! You've arrived at the totally legit website of Richard Dalrymple!</h3>
-            <p>I am a student, currently enrolled at Codeclan's Glasgow campus, 
-            undertaking the 16 week Software Development Course and having an absolute ball!.</p>
-            <p>Having worked in retail background for over ten years, I was eager for a change. </p>
+            <div className="home-text">
+                <h3>Hi! My name is Richard Dalrymple</h3>
+                <p>I am a student, currently enrolled at Codeclan's Glasgow campus, 
+                undertaking the 16 week Software Development Course and having an absolute ball!</p>
+                <p>Having worked in retail background for over ten years, I was eager for a change. </p>
+            </div>
+            <div className="home-image">
+                <img src="images/me.jpeg" height="100" width="300"></img>
+            </div>
         </div>
 
      );
