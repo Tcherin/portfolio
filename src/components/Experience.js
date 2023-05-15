b2a7b78c3f95722a51e5e5cad5e472bfddb92998
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-05-15 16:11:49 +0100

    imagestest 2

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import p from '../images/python.png';
-import logo from './logo512.png';
 
 
 
@@ -15,27 +13,27 @@ import logo from './logo512.png';
             </div>
             <div className="experience-content">
                 <div className="experience-python">
-                    <img src={p} height="125" width="125"></img>
+                    <img src="./portfolio/images/python.png"></img>
                     <p>Python</p>
                 </div>
                 <div className="experience-flask">
-                    <img src={logo} height="125" width="125"></img>
+                    <img src="./portfolio/images/flask.png" height="125" width="125"></img>
                     <p>Flask</p>
                 </div>
                 <div className="experience-html">
-                    <img src="./images/python.png" height="125" width="125"></img>
+                    <img src="./portfolio/images/html.png" height="125" width="125"></img>
                     <p>HTML</p>
                 </div>
                 <div className="experience-css">
-                    <img src="/images/css.png" height="125" width="125"></img>
+                    <img src="./portfolio/images/css.png" height="125" width="125"></img>
                     <p>CSS</p>
                 </div>
                 <div className="experience-javascript">
-                    <img src="/images/javascript.png" height="125" width="125"></img>
+                    <img src="./portfolio//images/javascript.png" height="125" width="125"></img>
                     <p>JavaScript</p>
                 </div>
                 <div className="experience-react">
-                    <img src="/images/react.png" height="125" width="125"></img>
+                    <img src="./portfolio/images/react.png" height="125" width="125"></img>
                     <p>React</p>
                 </div>
             </div>
