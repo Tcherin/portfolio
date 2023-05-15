bae1dabdeeb7c8f30b12457ac9b2c265fe1fbac9
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-05-15 16:06:12 +0100

    images test

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import p from '../images/python.png';
+import logo from './logo512.png';
+
 
 
 
@@ -12,27 +15,27 @@ import React from 'react';
             </div>
             <div className="experience-content">
                 <div className="experience-python">
-                    <img src="images/python.png" height="125" width="125"></img>
+                    <img src={p} height="125" width="125"></img>
                     <p>Python</p>
                 </div>
                 <div className="experience-flask">
-                    <img src="images/flask.png" height="125" width="125"></img>
+                    <img src={logo} height="125" width="125"></img>
                     <p>Flask</p>
                 </div>
                 <div className="experience-html">
-                    <img src="images/html.png" height="125" width="125"></img>
+                    <img src="./images/python.png" height="125" width="125"></img>
                     <p>HTML</p>
                 </div>
                 <div className="experience-css">
-                    <img src="images/css.png" height="125" width="125"></img>
+                    <img src="/images/css.png" height="125" width="125"></img>
                     <p>CSS</p>
                 </div>
                 <div className="experience-javascript">
-                    <img src="images/javascript.png" height="125" width="125"></img>
+                    <img src="/images/javascript.png" height="125" width="125"></img>
                     <p>JavaScript</p>
                 </div>
                 <div className="experience-react">
-                    <img src="images/react.png" height="125" width="125"></img>
+                    <img src="/images/react.png" height="125" width="125"></img>
                     <p>React</p>
                 </div>
             </div>
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -12,18 +12,18 @@ const Interests = () => {
                     this is what you'll find me doing!</p>
                 </div>
                 <div className="interests-content">
-                <img src='images/musical-theatre.jpg' width="450" height="350"></img>
+                <img src='/images/musical-theatre.jpg' width="450" height="350"></img>
                 <p>I enjoy performing with many of Glasgow's
                     amateur theatre companies. Discovering this lively scene gave me much more
                     confidence after moving to Glasgow to study. Whether its singing, dancing
                     or acting, I'm always keen to get stuck in!</p>
 
-                <img src="images/art.jpg" width="450" height="350"></img>
+                <img src="/images/art.jpg" width="450" height="350"></img>
                 <p>Over my first four years in Glasgow, I studied an honours degree
                     in Contemporary Art Practice. I still love to draw and paint and get a lot of enjoyment
                     putting that impulse into practice designing the front-ends of my coding projects.</p>
 
-                <img src="images/writing.jpg" width="450" height="350"></img>
+                <img src="/images/writing.jpg" width="450" height="350"></img>
                 <p>I have written several novels over the last few years, both in literary and genre fiction.
                     After completing my honours degree, these projects taught me a self-directed focus for long-term goals 
                     that has served me ever since.</p>
