e6d6c84369650fa8e131d38fd8627db1f1bac147
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-05-15 08:20:35 +0100

    testing terminal

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return ( 
         <div className='home-container'>
             <div className="home-text">
-                <h1>The Obligatory Intro</h1>
+                <h1>The Obligatory Introduction</h1>
                 <p>Hi! My name is Richard Dalrymple and I am an aspiring Software Engineer.</p> 
                 <p>Currently enrolled at Codeclan's Glasgow campus, I'm undertaking the 16 week Software 
                 Development Course and having an absolute ball!</p>
